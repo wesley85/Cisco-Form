7ba3caedf1160a0e85ce0bf87662b502b115fef4
Wesley Seitz <wseitz@west.com>
2019-09-23 12:22:48 -0600

    added comapny logo and changed color scheme

--- Cisco-Headset/src/components/Header.js
+++ Cisco-Headset/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Header = (props) => (
   <div className="header">
+  <img src="../../images/intrado-logo-wh-rev.png" className="logo-left"/>
   <div>
     <h1 className="header__title">{props.title}</h1>
     {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
