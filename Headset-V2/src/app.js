e7d9cfecd2906d1ce163b5a311343e5be0b33688
Wesley Seitz <wseitz@west.com>
2019-10-01 11:48:40 -0600

    Setting up server for V2 of headset app

--- Headset-V2/src/app.js
+++ Headset-V2/src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
 import HeadsetApp from './components/HeadsetApp';
 import 'normalize.css/normalize.css';
 
